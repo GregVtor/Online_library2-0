1bb75d24edf508620843dbde9006cfc6471f57a2
grisha <Vtorovstugreg@gmail.com>
2023-04-15 03:33:51 +0300

    upgrade register form

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import hashlib
 
 from flask import Flask, render_template, request, make_response
 from flask_login import LoginManager, login_user
+from json import dumps
 from db_con import db_init
 
 app = Flask(__name__)
@@ -10,10 +11,7 @@ app.app_context().push()
 db = db_init(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-secret_key = 'cf1cc4234b428b2ece18cd6f9f9613c6680580e23260c01ca4b66' \
-             '89f00af1a4fea69e166169e0c45c8e60112e6dc3e2d5a7a6fbccb78' \
-             '261f99b3ae404684f961'
-
+BASE_LIST = ['Classes', 'User']
 from models import *
 
 
@@ -50,7 +48,7 @@ def login_handler():
         res.headers['Content-Type'] = 'application/text'
         return res
 
-    load_user(db.session.query(User).filter_by(email=email).first().id)
+    login_user(db.session.query(User).filter_by(email=email).first().id)
 
     return '', 200
 
@@ -58,6 +56,20 @@ def login_handler():
 @app.route('/register', methods=['POST'])
 def register_handler():
     return ''
+
+
+@app.route('/db_req_classes', methods=['GET'])
+def db_req_handler():
+    classes = db.session.query(CLasses).all()
+    # if not classes:
+    #     return '', 200
+    classes_names = dumps({'cl': [i.name for i in classes]}, ensure_ascii=False)
+    res = make_response(classes_names, 200)
+    res.headers['Content-Type'] = 'application/json'
+    return res
+
+
+
 
 
 if __name__ == '__main__':
